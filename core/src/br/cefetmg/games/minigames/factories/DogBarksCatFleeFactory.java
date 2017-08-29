6e4fcf25e6da734e4eac97c36ba91a1b2e94a09b
Pedro_Belisario <pedrobelisario1997@gmail.com>
2017-08-29 19:23:47 -0300

    Bug
    Corrigindo bug do ultimo push pull

--- core/src/br/cefetmg/games/minigames/factories/DogBarksCatFleeFactory.java
+++ core/src/br/cefetmg/games/minigames/factories/DogBarksCatFleeFactory.java
@@ -1,35 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.cefetmg.games.minigames.factories;
 
-import br.cefetmg.games.minigames.DogBarksCatFlee;
-import br.cefetmg.games.minigames.MiniGame;
-import br.cefetmg.games.minigames.util.MiniGameStateObserver;
-import br.cefetmg.games.screens.BaseScreen;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Texture;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- *
- * @author Pedro
- */
-public class DogBarksCatFleeFactory implements MiniGameFactory{
-
-    @Override
-    public MiniGame createMiniGame(BaseScreen screen, 
-            MiniGameStateObserver observer, float difficulty) {
-        return new DogBarksCatFlee (screen, observer,difficulty);
-        
-    }
-
-    @Override
 package br.cefetmg.games.minigames.factories;
 
 import br.cefetmg.games.minigames.DogBarksCatFlee;
@@ -62,47 +31,5 @@ public class DogBarksCatFleeFactory implements MiniGameFactory {
         };
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.cefetmg.games.minigames.factories;
 
-import br.cefetmg.games.minigames.DogBarksCatFlee;
-import br.cefetmg.games.minigames.MiniGame;
-import br.cefetmg.games.minigames.ShooTheTartarus;
-import br.cefetmg.games.screens.BaseScreen;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Texture;
-import java.util.HashMap;
-import java.util.Map;
-import br.cefetmg.games.minigames.util.MiniGameStateObserver;
 
-/**
- *
- * @author Dota
- */
-public class DogBarksCatFleeFactory implements MiniGameFactory {
-    
-    @Override
-    public MiniGame createMiniGame(BaseScreen screen,
-            MiniGameStateObserver observer, float difficulty) {
-        return new DogBarksCatFlee(screen, observer, difficulty);
-    }
-
-    @Override
-    public Map<String, Class> getAssetsToPreload() {
-        return new HashMap<String, Class>() {
-            {put("DogBarksCatFlee/dog1", Texture.class);            
-             put("DogBarksCatFlee/tile0.png", Texture.class);
-             put("DogBarksCatFlee/tile1.png", Texture.class);
-             put("DogBarksCatFlee/tile2.png", Texture.class);
-             put("DogBarksCatFlee/tile3.png", Texture.class);
-             put("DogBarksCatFlee/tile4.png", Texture.class);
-            }
-        };
-    }
-}
->>>>>>> 82de1e0f2314dda58c4434f77c4f943190f0f7e7
