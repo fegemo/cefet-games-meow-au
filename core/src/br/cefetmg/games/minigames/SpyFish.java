01cba4ab2e7aefcea15cbb6c2d131ac310795d97
luizaas <luizanjosouza@gmail.com>
2017-08-29 23:21:03 -0300

    criando a classe SpyFish

---
+++ core/src/br/cefetmg/games/minigames/SpyFish.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.games.minigames;
+
+/**
+ *
+ * @author Alberto
+ */
+public class SpyFish extends MiniGame{
+
+    public SpyFish() {
+        super(null, null, 0, 0, null);
+    }
+
+    @Override
+    protected void challengeSolved() {
+        super.challengeSolved(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void onStart() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void configureDifficultyParameters(float difficulty) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onHandlePlayingInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onUpdate(float dt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onDrawGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getInstructions() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean shouldHideMousePointer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
