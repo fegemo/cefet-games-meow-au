bda7e8f8e5d3ccc85ed98bac7b97f3150e687199
luizaas <luizanjosouza@gmail.com>
2017-08-30 23:39:33 -0300

    sera q fiz merda?

--- core/src/br/cefetmg/games/minigames/SpyFish.java
+++ core/src/br/cefetmg/games/minigames/SpyFish.java
@@ -5,16 +5,48 @@
  */
 package br.cefetmg.games.minigames;
 
+import br.cefetmg.games.Config;
+import br.cefetmg.games.graphics.MultiAnimatedSprite;
+import br.cefetmg.games.minigames.util.MiniGameStateObserver;
+import br.cefetmg.games.minigames.util.TimeoutBehavior;
+import br.cefetmg.games.screens.BaseScreen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import java.util.HashMap;
+
 /**
  *
  * @author Alberto
  */
 public class SpyFish extends MiniGame{
-
-    public SpyFish() {
-        super(null, null, 0, 0, null);
+    //texturas
+    private Texture texturaFish;
+    private Texture texturaCard;
+    private Texture texturaFundo;
+    private Texture texturaFcontrol;
+    private Texture texturaMcontrol;
+    
+    //elementos de logica
+    private Fish fish;
+    private Control control;
+    private MemoryCard memoryCard;
+    
+    //elementos de dificuldade
+    
+    
+     public SpyFish(BaseScreen screen,
+          MiniGameStateObserver observer, float difficulty) {
+        super(screen, observer, difficulty,10000,
+                TimeoutBehavior.FAILS_WHEN_MINIGAME_ENDS);
+        fish=new Fish(texturaFish);
+        control = new Control();
     }
-
     @Override
     protected void challengeSolved() {
         super.challengeSolved(); //To change body of generated methods, choose Tools | Templates.
@@ -22,37 +54,110 @@ public class SpyFish extends MiniGame{
 
     @Override
     protected void onStart() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.texturaFish = assets.get("spy-fish/fish.png",Texture.class);
+        this.texturaCard = assets.get("spy-fish/memory-card.png",Texture.class);
+        this.texturaFundo = assets.get("spy-fish/fundo.png",Texture.class);
+        this.texturaFcontrol = assets.get("spy-fish/fundo-controle.png",Texture.class);
+        this.texturaMcontrol = assets.get("spy-fish/controle-principal.png",Texture.class);
+        
     }
 
     @Override
     protected void configureDifficultyParameters(float difficulty) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void onHandlePlayingInput() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Vector2 click = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+        viewport.unproject(click);
+        if(Gdx.input.isTouched())
+            control.update(click);
+        else
+            control.update();
+        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void onUpdate(float dt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        fish.update(dt);
+        memoryCard.update();
     }
 
     @Override
     public void onDrawGame() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        fish.draw(batch);
+        control.draw(batch);
+        memoryCard.draw(batch);
     }
 
     @Override
     public String getInstructions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "Pegue o cartão de memória";
     }
 
     @Override
     public boolean shouldHideMousePointer() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
+    //   throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    class Fish extends MultiAnimatedSprite{
+
+        private Vector2 dposi;
+        private int lado;
+
+        static final int FRAME_WIDTH = 28;
+        static final int FRAME_HEIGHT = 36;
+
+        public Fish(final Texture fishSprite) {
+            super(new HashMap<String, Animation>() {
+                {
+                    TextureRegion[][] frames = TextureRegion
+                            .split(fishSprite,
+                                    FRAME_WIDTH, FRAME_HEIGHT);
+                    Animation walking = new Animation(0.2f,
+                            frames[0][0],
+                            frames[0][1],
+                            frames[0][2],
+                            frames[0][1]);
+                    walking.setPlayMode(Animation.PlayMode.LOOP);
+                    put("walking", walking);
+                }
+            }, "walking");
+        }
+        @Override
+        public void update(float dt){
+            super.update(dt);
+            float auxX = dposi.x>0?min(Config.WORLD_WIDTH,super.getX()+dposi.x):max(0,super.getX()+dposi.x);
+            float auxY = dposi.y>0?min(Config.WORLD_HEIGHT,super.getY()+dposi.y):max(0,super.getY()+dposi.y);
+            super.setPosition(auxX, auxY);
+            setDposi(0, 0);
+        }
+        public void setDposi(float x, float y){
+            this.dposi.x=x;
+            this.dposi.y=y;
+        }
     }
     
+    class MemoryCard extends Sprite{
+
+        public MemoryCard() {
+        }
+        public void update(){}
+    }
+    
+    class Control extends Sprite{
+        private Texture fundo;
+        private Texture meio;
+        private Vector2 centro;
+        private Vector2 centroMeio;
+        
+        
+        public void update(Vector2 click){
+            //verifica se esta pressionando a bolinha de dentro se esta é possivel arrasta-la ate a borda da bola grande
+        }
+        public void update(){
+            centroMeio = centro;
+        }
+    }
 }
--- core/src/br/cefetmg/games/minigames/factories/SpyFishFactory.java
+++ core/src/br/cefetmg/games/minigames/factories/SpyFishFactory.java
@@ -6,8 +6,11 @@
 package br.cefetmg.games.minigames.factories;
 
 import br.cefetmg.games.minigames.MiniGame;
+import br.cefetmg.games.minigames.SpyFish;
 import br.cefetmg.games.minigames.util.MiniGameStateObserver;
 import br.cefetmg.games.screens.BaseScreen;
+import com.badlogic.gdx.graphics.Texture;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -18,12 +21,21 @@ public class SpyFishFactory implements MiniGameFactory{
 
     @Override
     public MiniGame createMiniGame(BaseScreen screen, MiniGameStateObserver observer, float difficulty) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new SpyFish(screen, observer, difficulty);
     }
 
     @Override
     public Map<String, Class> getAssetsToPreload() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new HashMap<String, Class>(){
+            {
+                put("spy-fish/fish.png", Texture.class );
+                put("spy-fish/memory-card.png", Texture.class);
+                put("spy-fish/fundo.png", Texture.class);
+                put("spy-fish/fundo-controle.png",Texture.class);
+                put("spy-fish/controle-principal.png",Texture.class);
+            }
+        };
+    //To change body of generated methods, choose Tools | Templates.
     }
     
 }
