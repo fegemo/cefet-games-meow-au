7b354c02ab88aebdafe143b050e7f408513ed982
natyiassanori <natsumyiass@gmail.com>
2017-09-03 12:57:49 -0300

    Adicionada classe secundária para tratar sprite

--- core/src/br/cefetmg/games/minigames/MouseAttack.java
+++ core/src/br/cefetmg/games/minigames/MouseAttack.java
@@ -13,6 +13,10 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Timer.Task;
 import br.cefetmg.games.minigames.util.Cat;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import net.dermetfan.gdx.graphics.g2d.AnimatedSprite;
 
 /**
  *
@@ -22,7 +26,7 @@ public class MouseAttack extends MiniGame {
 
     private Array<Sprite> enemies;
     private Texture catTexture;
-    private Cat cat;
+    private Cat2 cat;
 
 
     private int enemiesKilled;
@@ -43,35 +47,29 @@ public class MouseAttack extends MiniGame {
     public MouseAttack(BaseScreen screen,
                        MiniGameStateObserver observer, float difficulty) {
         super(screen, observer, difficulty, 10f,
-                TimeoutBehavior.FAILS_WHEN_MINIGAME_ENDS);
+                TimeoutBehavior.WINS_WHEN_MINIGAME_ENDS);
     }
 
     @Override
     protected void onStart() {
-        catTexture = assets.get ("MouseAttack/sprite-cat.png",Texture.class);
-        cat= new Cat(catTexture);
+        
+        catTexture = assets.get("MouseAttack/sprite-cat.png",Texture.class);
+
+        cat = new Cat2(catTexture);
+        
         ScreenHeight = Gdx.graphics.getHeight();
         ScreenWidth = Gdx.graphics.getWidth();
-        posX = (float) (ScreenWidth * 0.5);
-        posY = (float) (ScreenHeight * 0.5);
-        cat.setOriginCenter();;
-        enemies = new Array<Sprite>();
+        posX = (float)(ScreenWidth*0.2);
+        posY = (float)(ScreenHeight*0.5);
+        cat.setPosition(50, 50);
+        
+        timer.scheduleTask(new Task() {
+            @Override
+            public void run() {
+                spawnEnemy();
+            }
 
-
-        enemiesKilled = 0;
-        spawnedEnemies = 0;
-        scheduleEnemySpawn();
-
-        cariesTexture = assets.get(
-                "shoot-the-caries/caries.png", Texture.class);
-        targetTexture = assets.get(
-                "shoot-the-caries/target.png", Texture.class);
-        cariesAppearingSound = assets.get(
-                "shoot-the-caries/caries1.mp3", Sound.class);
-        cariesDyingSound = assets.get(
-                "shoot-the-caries/caries2.mp3", Sound.class);
-        target = new Sprite(targetTexture);
-        target.setOriginCenter();
+        }, 0, this.spawnInterval);
 
     }
 
@@ -93,7 +91,7 @@ public class MouseAttack extends MiniGame {
 
     private void spawnEnemy() {
         // pega x e y entre 0 e 1
-        Vector2 position = new Vector2(rand.nextFloat(), rand.nextFloat());
+        /*Vector2 position = new Vector2(rand.nextFloat(), rand.nextFloat());
         // multiplica x e y pela largura e altura da tela
         position.scl(
                 viewport.getWorldWidth() - cariesTexture.getWidth()
@@ -107,7 +105,7 @@ public class MouseAttack extends MiniGame {
         enemies.add(enemy);
 
         // toca um efeito sonoro
-        cariesAppearingSound.play(0.5f);
+        cariesAppearingSound.play(0.5f);*/
     }
 
     @Override
@@ -122,12 +120,12 @@ public class MouseAttack extends MiniGame {
     }
 
     @Override
-    public void onHandlePlayingInput() {
+    public void onHandlePlayingInput() { 
         Vector3 Posi;
         Posi = new Vector3(posX, posY, 0);
         viewport.unproject(Posi);
         cat.setCenter(Posi.x, Posi.y);
-
+/*
         // atualiza a posição do alvo de acordo com o mouse
         Vector3 click = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
         viewport.unproject(click);
@@ -159,20 +157,20 @@ public class MouseAttack extends MiniGame {
                     break;
                 }
             }
-        }
+        }*/
     }
 
     @Override
     public void onUpdate(float dt) {
         cat.update(dt);
         // vai diminuindo o tamanho das cáries existentes
-        for (int i = 0; i < enemies.size; i++) {
+        /*for (int i = 0; i < enemies.size; i++) {
             Sprite sprite = enemies.get(i);
             // diminui só até x% do tamanho da imagem
             if (sprite.getScaleX() > minimumEnemyScale) {
                 sprite.setScale(sprite.getScaleX() - 0.3f * dt);
             }
-        }
+        }*/
     }
 
     @Override
@@ -183,16 +181,73 @@ public class MouseAttack extends MiniGame {
     @Override
     public void onDrawGame() {
         cat.draw(batch);
-       for (int i = 0; i < enemies.size; i++) {
+       /*for (int i = 0; i < enemies.size; i++) {
             Sprite sprite = enemies.get(i);
             sprite.draw(batch);
         }
-        target.draw(batch);
+        target.draw(batch);*/
     }
 
     @Override
     public boolean shouldHideMousePointer() {
         return true;
+    }
+    
+    class Cat2 extends AnimatedSprite{
+        static final int FRAME_WIDTH = 50;
+        static final int FRAME_HEIGHT = 50;
+        TextureRegion[][] quadrosDaAnimacao;
+        Texture spriteSheet;
+        
+        
+        Animation chutar;
+        
+        public Cat2(final Texture cat) {
+            
+            /*this.spriteSheet = spriteSheet;
+        
+            spriteSheet = new Texture("MouseAttack/sprite-cat2.png");
+            
+            quadrosDaAnimacao = TextureRegion.split(spriteSheet, 50, 50);
+            chutar = new Animation(0.1f,
+            quadrosDaAnimacao[0][0], // 1ª linha, 1ª coluna
+            quadrosDaAnimacao[0][1], // idem, 2ª coluna
+            quadrosDaAnimacao[0][2],
+            quadrosDaAnimacao[0][3]);*/
+            
+             super(new Animation(0.1f, new Array<TextureRegion>() {
+                {
+                    TextureRegion[][] frames = TextureRegion.split(
+                            cat, 50, 50);
+                    super.addAll(new TextureRegion[]{
+                        frames[4][0],
+                        frames[4][1],
+                        frames[4][2],
+                        frames[4][3],
+                        frames[4][4],
+                        frames[4][5],
+                        frames[4][6],
+                        frames[4][7],
+                        frames[4][8]
+                    });
+                }
+            }));
+            super.getAnimation().setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+            //chutar.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+            super.setAutoUpdate(false);
+            //this.cat = cat;
+        }
+
+       
+        Vector2 getHeadPosition() {
+            return new Vector2(
+                    this.getX() + this.getWidth() * 0.5f,
+                    this.getY() + this.getHeight() * 0.8f);
+        }
+
+        float getHeadDistanceTo(float enemyX, float enemyY) {
+            return getHeadPosition().dst(enemyX, enemyY);
+        }
     }
 
 }
