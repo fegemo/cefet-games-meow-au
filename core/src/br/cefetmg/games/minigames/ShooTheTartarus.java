4429f73684a6b521e585896449801d0a204acc61
Flavio Coutinho <fegemo@gmail.com>
2017-09-25 15:31:00 -0300

    [fix] Resets an improper change to ShooTheTartarus and its factory

--- core/src/br/cefetmg/games/minigames/ShooTheTartarus.java
+++ core/src/br/cefetmg/games/minigames/ShooTheTartarus.java
@@ -346,8 +346,8 @@ public class ShooTheTartarus extends MiniGame {
         private final TextureRegion broken;
         private int lives = 2;
 
-        static final int FRAME_WIDTH = 32;
-        static final int FRAME_HEIGHT = 24;
+        static final int FRAME_WIDTH = 64;
+        static final int FRAME_HEIGHT = 64;
 
         public Tooth(TextureRegion textureOk, TextureRegion textureHurt,
                 TextureRegion textureBroken, int lives) {
