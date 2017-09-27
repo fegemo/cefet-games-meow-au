f3e64b7631eded80e8837e9e98f3106b7d82bb45
Vinicius Henrique Silveira <vhsilveira2@gmail.com>
2017-09-27 07:43:58 -0300

    correcoes

--- core/src/br/cefetmg/games/minigames/CatAvoider.java
+++ core/src/br/cefetmg/games/minigames/CatAvoider.java
@@ -4,6 +4,7 @@ import static br.cefetmg.games.Config.*;
 import br.cefetmg.games.minigames.util.DifficultyCurve;
 import br.cefetmg.games.graphics.MultiAnimatedSprite;
 import static br.cefetmg.games.minigames.CatAvoider.*;
+import static br.cefetmg.games.minigames.HeadSoccer.Player.convertToRad;
 import br.cefetmg.games.minigames.util.TimeoutBehavior;
 import br.cefetmg.games.screens.BaseScreen;
 import com.badlogic.gdx.Gdx;
@@ -27,59 +28,83 @@ import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
+import static java.lang.Math.abs;
 import java.util.Random;
 
-class AnimatedSprite {
-
-    public Animation moviment;
-    public float timeAnimation;
-    private Texture spriteSheet;
-    private TextureRegion[][] animationPictures;
-
-    public AnimatedSprite(String sprite, float time, int frameWidth, int frameHeight, int frames, int sense, int startPosiiton) {
-        spriteSheet = new Texture(sprite);
-        animationPictures = TextureRegion.split(spriteSheet, frameWidth, frameHeight);
-
-        timeAnimation = 0;
-        createMoviment(time, sense, frames, startPosiiton);
-    }
-
-    public void configurePlayMode(Animation.PlayMode p) {
-        moviment.setPlayMode(p);
-    }
-
-    public void createMoviment(float time, int sense, int frames, int startPosiiton) {
-        TextureRegion[] t = new TextureRegion[frames];
-        int i, k;
-        k = 0;
-        if (sense == LEFT) {
-            for (i = startPosiiton; i >= 0; i--) {
-                t[k] = animationPictures[0][i];
-                k++;
-            }
-            for (i = frames-1; i > startPosiiton; i--) {
-                t[k] = animationPictures[0][i];
-                k++;
-            }
-        } else {
-            for (i = startPosiiton; i < frames; i++) {
-                t[k] = animationPictures[0][i];
-                k++;
-            }
-            for (i = 0; i < startPosiiton; i++) {
-                t[k] = animationPictures[0][i];
-                k++;
-            }
+public class CatAvoider extends MiniGame {
+    class Obstacle {
+        private Texture color;
+        private Sprite visible;
+        private Vector2 position;
+        private Rectangle rec;
+        private SpriteBatch batch;
+        private static final float friction = 2;
+        public Obstacle(SpriteBatch batch, Vector2 position, float width, float height) {
+            this.color = new Texture("avoider/grey.png");
+            this.batch = batch;
+            this.position = position;
+            rec = new Rectangle(this.position.x, this.position.y, width, height);
+            visible = new Sprite(color);
+            visible.setPosition(this.position.x, this.position.y);
+            visible.setSize(width, height);
         }
 
-        moviment = new Animation(time, t);
-        moviment.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+        public void draw() {
+            visible.draw(batch);
+        }
+
+        public Rectangle getRec() {
+            return rec;
+        }
     }
-}
+    
+    class AnimatedSprite {
+        public Animation moviment;
+        public float timeAnimation;
+        private Texture spriteSheet;
+        private TextureRegion[][] animationPictures;
 
+        public AnimatedSprite(String sprite, float time, int frameWidth, int frameHeight, int frames, int sense, int startPosiiton) {
+            spriteSheet = new Texture(sprite);
+            animationPictures = TextureRegion.split(spriteSheet, frameWidth, frameHeight);
 
-public class CatAvoider extends MiniGame {
+            timeAnimation = 0;
+            createMoviment(time, sense, frames, startPosiiton);
+        }
 
+        public void configurePlayMode(Animation.PlayMode p) {
+            moviment.setPlayMode(p);
+        }
+
+        public void createMoviment(float time, int sense, int frames, int startPosiiton) {
+            TextureRegion[] t = new TextureRegion[frames];
+            int i, k;
+            k = 0;
+            if (sense == LEFT) {
+                for (i = startPosiiton; i >= 0; i--) {
+                    t[k] = animationPictures[0][i];
+                    k++;
+                }
+                for (i = frames-1; i > startPosiiton; i--) {
+                    t[k] = animationPictures[0][i];
+                    k++;
+                }
+            } else {
+                for (i = startPosiiton; i < frames; i++) {
+                    t[k] = animationPictures[0][i];
+                    k++;
+                }
+                for (i = 0; i < startPosiiton; i++) {
+                    t[k] = animationPictures[0][i];
+                    k++;
+                }
+            }
+
+            moviment = new Animation(time, t);
+            moviment.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+        }
+    }
+    
     public static final int NORTH = 0;
     public static final int SOUTH = 1;
     public static final int WEST = 2;
@@ -131,6 +156,117 @@ public class CatAvoider extends MiniGame {
 
     protected Wool wool = new Wool();
 
+    static class Colision {
+        /**
+         * Verifica se dois círculos em 2D estão colidindo.
+         *
+         * @param c1 círculo 1
+         * @param c2 círculo 2
+         * @return true se há colisão ou false, do contrário.
+         */
+        public static final boolean circlesOverlap(Circle c1, Circle c2) {
+            Vector2 v1, v2;
+            v1 = new Vector2(c1.x, c1.y);
+            v2 = new Vector2(c2.x, c2.y);
+            double distance2 = v1.dst2(v2);
+            if (distance2 > ((c1.radius + c2.radius) * (c1.radius + c2.radius))) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        /**
+         * Verifica se dois retângulos em 2D estão colidindo. Esta função pode
+         * verificar se o eixo X dos dois objetos está colidindo e então se o mesmo
+         * ocorre com o eixo Y.
+         *
+         * @param r1 retângulo 1
+         * @param r2 retângulo 2
+         * @return true se há colisão ou false, do contrário.
+         */
+        public static final boolean rectsOverlap(Rectangle r1, Rectangle r2) {
+            float r1XMin = r1.x, r1XMax = r1.x + r1.width, r1YMin = r1.y, r1YMax = r1.y + r1.height;
+            float r2XMin = r2.x, r2XMax = r2.x + r2.width, r2YMin = r2.y, r2YMax = r2.y + r2.height;
+
+            if ((r1XMax >= r2XMin && r1XMin <= r2XMax) && (r1YMax >= r2YMin && r1YMin <= r2YMax)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static final Vector2 rectCircleOverlap(Rectangle r1, Circle c1) {
+            float r1XCenter = r1.x + (r1.width / 2), r1YCenter = r1.y + (r1.height / 2);
+            float horizontalDistance = abs(c1.x - r1XCenter);
+            float verticalDistance = abs(c1.y - r1YCenter);
+            float pXCoordinate;
+            float pYCoordinate;
+
+            //Clamp operation
+            if (horizontalDistance > (r1.width / 2)) {
+                horizontalDistance = r1.width / 2;
+            }
+            if (verticalDistance > (r1.height / 2)) {
+                verticalDistance = r1.height / 2;
+            }
+
+            //Finding p Point Coordinates
+            if (c1.x > r1XCenter) {
+                pXCoordinate = r1XCenter + horizontalDistance;
+            } else {
+                pXCoordinate = r1XCenter - horizontalDistance;
+            }
+            if (c1.y > r1YCenter) {
+                pYCoordinate = r1YCenter + verticalDistance;
+            } else {
+                pYCoordinate = r1YCenter - verticalDistance;
+            }
+
+            Vector2 c1Center = new Vector2(c1.x, c1.y);
+            Vector2 pPoint = new Vector2(pXCoordinate, pYCoordinate);
+
+            float distance2 = c1Center.dst2(pPoint);
+
+            if (distance2 <= (c1.radius * c1.radius)) {
+                return pPoint;
+            } else {
+                return null;
+            }
+        }
+
+        public static boolean collideCircleWithRotatedRectangle(Circle c, Rectangle r, float rotation) {
+
+            Vector2 rectCenter = new Vector2();
+            r.getCenter(rectCenter);
+
+            Vector2 unrotatedCircle = new Vector2();
+
+            unrotatedCircle.x = (float) (Math.cos(rotation * convertToRad) * (c.x - rectCenter.x) - Math.sin(rotation * convertToRad) * (c.y - rectCenter.y) + rectCenter.x);
+            unrotatedCircle.y = (float) (Math.sin(rotation * convertToRad) * (c.x - rectCenter.x) + Math.cos(rotation * convertToRad) * (c.y - rectCenter.y) + rectCenter.y);
+
+            Vector2 closestPoint = new Vector2();
+
+            if (unrotatedCircle.x < r.x) {
+                closestPoint.x = r.x;
+            } else if (unrotatedCircle.x > r.x + r.width) {
+                closestPoint.x = r.x + r.width;
+            } else {
+                closestPoint.x = unrotatedCircle.x;
+            }
+
+            if (unrotatedCircle.y < r.y) {
+                closestPoint.y = r.y;
+            } else if (unrotatedCircle.y > r.y + r.height) {
+                closestPoint.y = r.y + r.height;
+            } else {
+                closestPoint.y = unrotatedCircle.y;
+            }
+
+            return unrotatedCircle.dst(closestPoint) < c.radius;
+        }
+    }
+    
     class Cat {
         protected Rectangle rect;//rectangle to enclose the cat and treat the collision
         protected Texture texture;//texture for the non playable character ninja cat
--- core/src/br/cefetmg/games/minigames/HeadSoccer.java
+++ core/src/br/cefetmg/games/minigames/HeadSoccer.java
@@ -1,11 +1,13 @@
 package br.cefetmg.games.minigames;
 
 import static br.cefetmg.games.Config.*;
+import br.cefetmg.games.minigames.CatAvoider.Colision;
 import static br.cefetmg.games.minigames.HeadSoccer.Player.FLOOR;
 import static br.cefetmg.games.minigames.HeadSoccer.Player.GRAVITY;
 import static br.cefetmg.games.minigames.HeadSoccer.Player.INITIALXLEFTGOAL;
 import static br.cefetmg.games.minigames.HeadSoccer.Player.INITIALXRIGHTGOAL;
 import static br.cefetmg.games.minigames.HeadSoccer.Player.INITIALYGOAL;
+import static br.cefetmg.games.minigames.HeadSoccer.Player.JUMP;
 import static br.cefetmg.games.minigames.HeadSoccer.Player.convertToRad;
 import br.cefetmg.games.minigames.util.DifficultyCurve;
 import br.cefetmg.games.minigames.util.TimeoutBehavior;
@@ -28,23 +30,360 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import javax.print.attribute.standard.PrinterStateReason;
 
-/**
- *
- * @author Flávio Coutinho - fegemo <coutinho@decom.cefetmg.br>
- */
-public class HeadSoccer extends MiniGame {
 
-        class Player {
-        public static final int WORLD_WIDTH = 1280;
-    public static final int WORLD_HEIGHT = 720;
-    public static final int FLOOR = 81;
-    public static final int JUMP = 7;
-    public static final int GRAVITY = 10;
-    public static final float convertToRad = 3.14159265359f/180;
+public class HeadSoccer extends MiniGame {
+    class Obstacle {
+        private Texture color;
+        private Sprite visible;
+        private Vector2 position;
+        private Rectangle rec;
+        private SpriteBatch batch;
+        private static final float friction = 2;
+        public Obstacle(SpriteBatch batch, Vector2 position, float width, float height) {
+            this.color = new Texture("avoider/grey.png");
+            this.batch = batch;
+            this.position = position;
+            rec = new Rectangle(this.position.x, this.position.y, width, height);
+            visible = new Sprite(color);
+            visible.setPosition(this.position.x, this.position.y);
+            visible.setSize(width, height);
+        }
+
+        public void draw() {
+            visible.draw(batch);
+        }
+
+        public Rectangle getRec() {
+            return rec;
+        }
+    }
     
-    public static final int INITIALXLEFTGOAL = -45;
-    public static final int INITIALXRIGHTGOAL = 1135;
-    public static final int INITIALYGOAL = 75;
+    class Bot {
+        public boolean walking, up, down, left, right, orientation, footUp, footDown, movingFoot, jump;
+        private float botStep, playerWidth, playerHeight;
+        public Sprite sprite_Bot, sprite_Shoes;
+        public Vector2 position, positionMin, positionMax, speed, footPosition;
+        private Texture botTexture;
+        private SpriteBatch batch;
+        public float maxSpeed;
+        private float mass, aTime;
+        private float rotation_angle,initial_angle, final_angle, correctionX, correctionY, aCorrectionX, aCorrectionY;
+        public float kick_power;
+        private int moveAlone;
+        private int oldOrientation;
+        private float oldTime, decisionPeriod;
+
+        public Bot(Vector2 positionInicial, Vector2 positionMin, Vector2 positionMax, Texture botTexture, SpriteBatch batch, float botStep, float maxSpeed, float sizeX, float sizeY, float mass) {
+            this.speed = new Vector2(0, 0);
+            this.walking = false;
+            this.positionMin = positionMin;
+            this.positionMax = positionMax;
+            this.batch = batch;
+            this.botStep = botStep;
+            this.botTexture = botTexture;
+            this.maxSpeed = maxSpeed;
+            this.mass = mass;
+            aTime = 0.3f;
+            footUp = false;
+            footDown = false;
+            movingFoot = false;
+            jump = false;
+            kick_power = 20;
+            oldTime = 0;
+            decisionPeriod = 0.2f;
+            this.sprite_Bot = new Sprite(botTexture);
+            this.sprite_Bot.setSize(sizeX, sizeY);
+            this.sprite_Bot.setFlip(false, false);
+            this.sprite_Shoes = new Sprite(new Texture("head-soccer/shoes.png"));
+            this.sprite_Shoes.setSize(0.7f * sizeX, 0.3f * sizeY);
+            this.sprite_Shoes.setOriginCenter();
+            this.sprite_Shoes.setRotation(27);
+            this.sprite_Shoes.setFlip(true, false);
+            this.initial_angle = -27;
+            final_angle = 45;
+
+            correctionY = -sprite_Shoes.getHeight()/2;
+
+            aCorrectionX = 0;
+            aCorrectionY = 0;
+
+            oldOrientation = 0;
+            this.playerWidth = sprite_Bot.getWidth();
+            this.playerHeight = sprite_Bot.getHeight();
+
+            this.position = positionInicial;
+            footPosition = new Vector2(positionInicial.x, positionInicial.y - sprite_Shoes.getHeight() / 2) ;
+
+            orientation = false;//true= Direita false = left
+            correctionX = sprite_Bot.getWidth()/3;
+
+            sprite_Bot.setPosition(positionInicial.x, positionInicial.y);
+            sprite_Shoes.setPosition(footPosition.x,footPosition.y);
+        }
+
+        public Sprite getSprite_Bot() {
+            return sprite_Bot;
+        }
+
+        public float getRotation_angle() {
+            return rotation_angle;
+        }
+        public void changeDificulty(float difficulty){
+            maxSpeed = maxSpeed * difficulty/10;
+            botStep = botStep * difficulty/10;
+            kick_power = kick_power * difficulty/10;
+        }
+        public void moveFoot(float dt) {
+            if (oldOrientation == 0 || (oldOrientation == 1 && orientation)
+                    || (oldOrientation == 2 && orientation == false)) {
+                if (movingFoot == true) {
+
+                    if (oldOrientation == 0) {
+                        if (orientation == true) {
+                            oldOrientation = 1;
+                        } else {
+                            oldOrientation = 2;
+                        }
+                    }
+
+                    float prop = dt / aTime;
+                    float limitX = playerWidth/1.5f;
+                    float limitY = 20;
+                    float speedUp = 2.5f;
+                    rotation_angle = sprite_Shoes.getRotation();
+                    if (orientation == true) {
+                        if (footDown == false) {
+                            if (rotation_angle - prop * speedUp * initial_angle < final_angle) {
+                                sprite_Shoes.setRotation(rotation_angle - prop * speedUp * initial_angle );
+                                 if(aCorrectionX + prop * limitX < limitX)
+                                    aCorrectionX += prop * limitX;
+                                 if(aCorrectionY + prop * limitY < limitY)
+                                      aCorrectionY += prop * limitY;    
+                            } else {
+                                aCorrectionX = limitX;
+                                aCorrectionY = limitY;
+                                sprite_Shoes.setRotation(final_angle);
+                                footDown = true;
+                            }
+                        } else {
+                            if (rotation_angle + prop * speedUp * initial_angle > initial_angle) {
+                                sprite_Shoes.setRotation(rotation_angle + prop * speedUp * initial_angle);
+                                if(aCorrectionX - prop * limitX > 0)
+                                    aCorrectionX -= prop * limitX;
+                                 if(aCorrectionY - prop * limitY > 0)
+                                      aCorrectionY -= prop * limitY;  
+                            } else {
+                                aCorrectionX = 0;
+                                aCorrectionY = 0;
+                                sprite_Shoes.setRotation(initial_angle);
+                                footUp = true;
+                            }
+                        }
+                    } 
+
+                    else {
+                        if (footDown == false) {
+                            if (rotation_angle +  prop * speedUp * initial_angle > -final_angle) {
+                                sprite_Shoes.setRotation(rotation_angle + prop * speedUp * initial_angle);
+                                if(aCorrectionX - prop * limitX > -limitX)
+                                    aCorrectionX -= prop * limitX;
+                                 if(aCorrectionY + prop * limitY < limitY)
+                                      aCorrectionY += prop * limitY;  
+                            } else {
+                                aCorrectionX = -limitX;
+                                aCorrectionY = limitY;
+                                sprite_Shoes.setRotation(-final_angle);
+                                footDown = true;
+                            }
+                        } else {
+                            if (rotation_angle - prop * speedUp * initial_angle < -initial_angle) {
+                                sprite_Shoes.setRotation(rotation_angle - prop * speedUp * initial_angle);  
+                                if(aCorrectionX + prop * limitX < 0)
+                                    aCorrectionX += prop * limitX;
+                                 if(aCorrectionY - prop * limitY > 0)
+                                      aCorrectionY -= prop * limitY;  
+                            } else {
+                                aCorrectionX = 0;
+                                aCorrectionY = 0;
+                                sprite_Shoes.setRotation(-initial_angle);
+                                footUp = true;
+                            }
+                        }
+
+                    }
+                    if (footUp) {
+                        footUp = false;
+                        footDown = false;
+                        movingFoot = false;
+                        oldOrientation = 0;
+                    }
+
+                }
+            } else {
+                footUp = false;
+                footDown = false;
+                movingFoot = false;
+                oldOrientation = 0;
+                aCorrectionX = 0;
+                aCorrectionY = 0;
+                if (orientation) {
+                    sprite_Shoes.setRotation(initial_angle);
+                } else {
+                    sprite_Shoes.setRotation(-initial_angle);
+                }
+            }
+        }
+
+        public Sprite getSprite_Shoes() {
+            return sprite_Shoes;
+        }
+
+        public void updateState(float dt, Vector2 ball, Vector2 player){
+            if(oldTime > 0)
+                oldTime += dt;
+            if(oldTime>decisionPeriod){
+                oldTime = 0;
+            }
+            if(oldTime == 0){
+                float x = sprite_Bot.getX();
+                float y = sprite_Bot.getY() + playerHeight;
+
+                if(ball.x > x + playerWidth && ball.y >y){
+                    moveAlone = 1;
+                }
+                else if(ball.x > x  && ball.y <= y){
+                    moveAlone = 2;
+                }
+                else if(ball.x < x && ball.y > y){
+                    moveAlone = 3;
+                }
+                else{
+                    moveAlone = 4;
+                }
+                if(Math.abs(ball.x-x) < playerWidth && movingFoot == false && ball.x < x ){
+                    movingFoot = true;
+                }
+                if(Math.abs(ball.x-x) < playerWidth/4 && ball.x > x)
+                    jump = true;
+            }
+        }  
+
+        public void updateMoviment(float dt, Vector2 ball, Vector2 player) {
+            updateState(dt,ball,player);
+            float x = sprite_Bot.getX(), y = sprite_Bot.getY();
+            rotation_angle = sprite_Shoes.getRotation();
+            walking = false;
+            up = false;
+            down = false;
+            left = false;
+            right = false;
+            switch (moveAlone) {
+                case 3:
+                case 4:
+                    if (speed.x > -1 * maxSpeed) {
+                        speed.x -= 1;
+                    }   if (orientation == true) {
+                        this.sprite_Bot.setFlip(false, false);
+                        this.sprite_Shoes.setFlip(true, false);
+                        this.sprite_Shoes.setRotation(27);
+                        correctionX = sprite_Bot.getWidth() / 3;
+
+                        orientation = false;
+                    }   break;
+                case 1:
+                case 2:
+                    if (speed.x < maxSpeed) {
+                        speed.x += 1;
+                    }   if (orientation == false) {
+                        correctionX = 0;
+                        this.sprite_Bot.setFlip(true, false);
+                        this.sprite_Shoes.setFlip(false, false);
+                        this.sprite_Shoes.setRotation(-27);
+                        orientation = true;
+                    }   break;
+                default:
+                    speed.x = 0;
+                    break;
+            }
+
+            if (moveAlone == 1 || moveAlone == 3 || jump == true) {
+                if(sprite_Bot.getY() == FLOOR)
+                    speed.y = JUMP;
+            }
+
+            float botStepx = botStep * (speed.x / maxSpeed);
+            float botStepy = botStep * (speed.y / maxSpeed);
+
+            if (speed.x < 0) {
+                if (x + botStepx > positionMin.x) {
+                    x += botStepx;
+                }
+                walking = true;
+                left = true;
+            } else if (speed.x > 0) {
+                if (x + botStepx < positionMax.x - playerWidth) {
+                    x += botStepx;
+                }
+                walking = true;
+                right = true;
+            }
+
+            if (y > FLOOR) {
+                if (speed.y > 0) {
+                    y += botStepy;
+                    walking = true;
+                    up = true;
+                } else if (speed.y < 0) {
+                    y += botStepy;
+                    walking = true;
+                    down = true;
+
+                }
+            } else if (y == FLOOR) {
+                if (speed.y > 0) {
+                    y += botStepy;
+                    walking = true;
+                    up = true;
+                }
+            }
+
+            if (y < FLOOR) {
+                y = FLOOR;
+            }
+
+            sprite_Bot.setPosition(x, y);
+            sprite_Shoes.setPosition(x + correctionX + aCorrectionX, y - sprite_Shoes.getHeight() / 2 + correctionY + aCorrectionY);
+        }
+
+        public void actionGravity(float value) {
+            speed.set(speed.x, speed.y - value);
+        }
+
+        public void draw() {
+            sprite_Bot.draw(batch);
+            sprite_Shoes.draw(batch);
+        }
+
+        public Vector2 getSpeed() {
+            return speed;
+        }
+
+        public float getMass() {
+            return mass;
+        }
+    }
+    
+    class Player {
+        public static final int WORLD_WIDTH = 1280;
+        public static final int WORLD_HEIGHT = 720;
+        public static final int FLOOR = 81;
+        public static final int JUMP = 7;
+        public static final int GRAVITY = 10;
+        public static final float convertToRad = 3.14159265359f/180;
+
+        public static final int INITIALXLEFTGOAL = -45;
+        public static final int INITIALXRIGHTGOAL = 1135;
+        public static final int INITIALYGOAL = 75;
             
         public boolean walking, orientation, footUp, footDown, movingFoot, left, right, jump ,kick;
         private float playerStep, playerWidth, playerHeight;
@@ -318,7 +657,6 @@ public class HeadSoccer extends MiniGame {
         public float getMass() {
             return mass;
         }
-
     }
     
     private boolean goalP, goalB;
