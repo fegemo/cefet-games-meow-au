36d2a73fdc99780e251ae92401b411128c8a79bf
Flavio Coutinho <fegemo@gmail.com>
2017-10-25 16:32:19 -0200

    Fixes (again) what had been fixed on UnderwaterCat

--- core/src/br/cefetmg/games/minigames/UnderwaterCat.java
+++ core/src/br/cefetmg/games/minigames/UnderwaterCat.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.cefetmg.games.minigames;
 
 import br.cefetmg.games.minigames.util.DifficultyCurve;
@@ -23,12 +18,12 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.math.Rectangle;
 
-
 /**
  *
  * @author Adriel
  */
 public class UnderwaterCat extends MiniGame {
+
     private float minimumEnemySpeed;
     private float maximumEnemySpeed;
     private float spawnInterval;
@@ -37,8 +32,7 @@ public class UnderwaterCat extends MiniGame {
     private Music backgroundMusic;
     private Music swimmingAmbientSound;
     private Sound gotFishSound;
-    
-    
+
     Animation<TextureRegion> swimmingAnimation;
     private Array<Fish> toCapture;
     private Array<Spiky> enemies;
@@ -48,38 +42,37 @@ public class UnderwaterCat extends MiniGame {
     private Texture fish3Texture;
     private Texture spikyTexture;
     TheCat mainCharacter;
-    
+
     private boolean isOver;
-    
+
     private int numberEaten = 0;
     private int fishToEat;
     private float time = 0;
-    
-    
+
     public UnderwaterCat(BaseScreen screen,
             MiniGameStateObserver observer, float difficulty) {
         super(screen, observer, difficulty, 10f,
                 TimeoutBehavior.FAILS_WHEN_MINIGAME_ENDS);
     }
-    
+
     @Override
-    protected void onStart() {  
+    protected void onStart() {
         backgroundMusic = assets.get("underwater-cat/water.mp3", Music.class);
         swimmingAmbientSound = assets.get("underwater-cat/swim.wav", Music.class);
-        gotFishSound = assets.get("underwater-cat/eat.wav",Sound.class);
+        gotFishSound = assets.get("underwater-cat/eat.wav", Sound.class);
         swimCatTexture = assets.get(
-                "underwater-cat/swimcatspritesheet.png", Texture.class);   
+                "underwater-cat/swimcatspritesheet.png", Texture.class);
         fish1Texture = assets.get(
                 "underwater-cat/fish1.png", Texture.class);
         fish2Texture = assets.get(
                 "underwater-cat/fish2.png", Texture.class);
         fish3Texture = assets.get(
                 "underwater-cat/fish3.png", Texture.class);
-        
+
         spikyTexture = assets.get(
                 "underwater-cat/fish5.png", Texture.class);
-        
-        mainCharacter  = new TheCat(swimCatTexture);
+
+        mainCharacter = new TheCat(swimCatTexture);
         toCapture = new Array<Fish>();
         enemies = new Array<Spiky>();
         background = assets.get(
@@ -88,14 +81,15 @@ public class UnderwaterCat extends MiniGame {
         isOver = false;
         this.time = 0;
         initializeFish(fishToEat);
-               timer.scheduleTask(new Timer.Task() {
+        timer.scheduleTask(new Timer.Task() {
             @Override
             public void run() {
                 spawnEnemy();
             }
         }, 0, this.spawnInterval);
     }
-        @Override
+
+    @Override
     protected void configureDifficultyParameters(float difficulty) {
         this.minimumEnemySpeed = DifficultyCurve.LINEAR
                 .getCurveValueBetween(difficulty, 120, 220);
@@ -106,9 +100,7 @@ public class UnderwaterCat extends MiniGame {
         this.fishToEat = (int) Math.ceil(DifficultyCurve.LINEAR
                 .getCurveValueBetween(difficulty, 2, 5)) + 1;
     }
-    
-    
-    
+
     @Override
     public String getInstructions() {
         return "Mate a fome, mas sem espinhos!";
@@ -118,162 +110,161 @@ public class UnderwaterCat extends MiniGame {
     public boolean shouldHideMousePointer() {
         return true;
     }
-   
-        
-            @Override
+
+    @Override
     public void onDrawGame() {
         backgroundMusic.setLooping(true);
         backgroundMusic.play();
-        
+
         batch.draw(background, 0, 0,
                 viewport.getWorldWidth(),
                 viewport.getWorldHeight());
-       for (Fish fish : this.toCapture) {
+        for (Fish fish : this.toCapture) {
             fish.draw(batch);
         }
-       for (Spiky s : this.enemies) {
+        for (Spiky s : this.enemies) {
             s.draw(batch);
         }
         mainCharacter.draw(batch);
         swimmingAmbientSound.setLooping(true);
         swimmingAmbientSound.play();
-        
-        if(isOver){
+
+        if (isOver) {
             backgroundMusic.stop();
             swimmingAmbientSound.stop();
         }
-        
-     }
-    
-        @Override
+
+    }
+
+    @Override
     public void onUpdate(float dt) {
         // atualiza a animação  do gato 
         mainCharacter.update(dt);
         // atualiza a animação  dos peixes
-       for (Fish fish : this.toCapture) {
+        for (Fish fish : this.toCapture) {
             fish.update(dt);
         }
-          
-         // verifica se personagemPrincipalEstáSobreAlgumPeixe        
-         for (int i = 0; i < this.toCapture.size; i++) {
+
+        // verifica se personagemPrincipalEstáSobreAlgumPeixe        
+        for (int i = 0; i < this.toCapture.size; i++) {
             Fish f = this.toCapture.get(i);
             f.update(dt);
-                if (f.getBoundingRectangle()
-                        .overlaps(mainCharacter.getBoundingRectangle())) { 
-                    toCapture.removeValue(toCapture.get(i), true);
-                    eatFish();
-                }
+            if (f.getBoundingRectangle()
+                    .overlaps(mainCharacter.getBoundingRectangle())) {
+                toCapture.removeValue(toCapture.get(i), true);
+                eatFish();
             }
-         
+        }
+
         // verifica se este inimigo está colidindo com personagem principal        
-         for (int i = 0; i < this.enemies.size; i++) {
+        for (int i = 0; i < this.enemies.size; i++) {
             Spiky s = this.enemies.get(i);
             s.update(dt);
             Rectangle bounds = mainCharacter.getBoundingRectangle();
-              bounds.setHeight(bounds.getHeight() - 50);
-              bounds.setWidth(bounds.getWidth() - 70);
-                if (s.getBoundingRectangle()
-                    .overlaps(bounds)) 
-                {
-                     isOver = true;
-                     super.challengeFailed();
-                }
-            }
-    }
-    
-    private void eatFish(){
-      numberEaten++;
-      gotFishSound.play();
-            if(numberEaten == fishToEat){
+            bounds.setHeight(bounds.getHeight() - 50);
+            bounds.setWidth(bounds.getWidth() - 70);
+            if (s.getBoundingRectangle()
+                    .overlaps(bounds)) {
                 isOver = true;
-                super.challengeSolved();
-            } 
+                super.challengeFailed();
+            }
+        }
     }
-    
-     @Override
-    protected void onEnd(){
+
+    private void eatFish() {
+        numberEaten++;
+        gotFishSound.play();
+        if (numberEaten == fishToEat) {
             isOver = true;
-            backgroundMusic.stop();
-            swimmingAmbientSound.stop(); 
+            super.challengeSolved();
+        }
     }
-    
-     @Override
+
+    @Override
+    protected void onEnd() {
+        isOver = true;
+        backgroundMusic.stop();
+        swimmingAmbientSound.stop();
+    }
+
+    @Override
     public void onHandlePlayingInput() {
         // atualiza a posição do alvo de acordo com o mouse
-            Vector3 click = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-            viewport.unproject(click);
-            mainCharacter.setCenter(click.x, click.y);
-            if (this.numberEaten >= this.fishToEat) {
-                        super.challengeSolved();
-                    }
-       }
-    
-    
-    private TextureRegion[] workFrames(final Texture texture,int rows, int columns){
-    TextureRegion[][] tmp = TextureRegion.split(texture, 
-				texture.getWidth() / columns,
-				texture.getHeight() / rows);
-    
-    TextureRegion[] swimFrames = new TextureRegion[columns * rows];
-		int index = 0;
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				swimFrames[index++] = tmp[i][j];
-			}
-		}
-     return swimFrames;
-     
+        Vector3 click = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        viewport.unproject(click);
+        mainCharacter.setCenter(click.x, click.y);
+        if (this.numberEaten >= this.fishToEat) {
+            super.challengeSolved();
+        }
     }
-    
-    private void initializeFish(int qtdePeixes){
-       int tipoPeixe = 1;
-       Vector3 posicao = new Vector3();
-       for(int i=0;i<=qtdePeixes;i++){
-       switch(tipoPeixe){
-           case 1:
-               createFish(fish1Texture,tipoPeixe);break;
-           case 2:
-               createFish(fish2Texture,tipoPeixe);break;
-           case 3:
-               createFish(fish3Texture,tipoPeixe);break;
-       }
-       if(tipoPeixe<=3)
-           tipoPeixe ++;
-       else
-           tipoPeixe=1;
-       }
-    }
-    
-    
-    private void createFish(Texture spritesFish,int tipo){     
-        
-           Fish fish;
-           if(tipo == 1){
-               fish = new Fish(spritesFish,6,8);
-               createFishAux(fish);
-           }
-           if(tipo == 2){
-               fish = new Fish(spritesFish,7,7);
-               createFishAux(fish);
-           }
-           if(tipo == 3){
-               fish = new Fish(spritesFish,8,6);
-               createFishAux(fish);
-           }
-            
+
+    private TextureRegion[] workFrames(final Texture texture, int rows, int columns) {
+        TextureRegion[][] tmp = TextureRegion.split(texture,
+                texture.getWidth() / columns,
+                texture.getHeight() / rows);
+
+        TextureRegion[] swimFrames = new TextureRegion[columns * rows];
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                swimFrames[index++] = tmp[i][j];
+            }
+        }
+        return swimFrames;
 
     }
-    
-    private void createFishAux(Fish fish){
-    Vector3 posicao = new Vector3();
-           float randomWidth = MathUtils.random(viewport.getWorldWidth() - 200, 200);
-           float randomHeight = MathUtils.random(viewport.getWorldHeight() - 200, 200);
 
-           fish.setCenter(randomWidth, randomHeight);
-           this.toCapture.add(fish);  
+    private void initializeFish(int qtdePeixes) {
+        int tipoPeixe = 1;
+        Vector3 posicao = new Vector3();
+        for (int i = 0; i <= qtdePeixes; i++) {
+            switch (tipoPeixe) {
+                case 1:
+                    createFish(fish1Texture, tipoPeixe);
+                    break;
+                case 2:
+                    createFish(fish2Texture, tipoPeixe);
+                    break;
+                case 3:
+                    createFish(fish3Texture, tipoPeixe);
+                    break;
+            }
+            if (tipoPeixe <= 3) {
+                tipoPeixe++;
+            } else {
+                tipoPeixe = 1;
+            }
+        }
     }
-   
-     private void spawnEnemy() {
+
+    private void createFish(Texture spritesFish, int tipo) {
+
+        Fish fish;
+        if (tipo == 1) {
+            fish = new Fish(spritesFish, 6, 8);
+            createFishAux(fish);
+        }
+        if (tipo == 2) {
+            fish = new Fish(spritesFish, 7, 7);
+            createFishAux(fish);
+        }
+        if (tipo == 3) {
+            fish = new Fish(spritesFish, 8, 6);
+            createFishAux(fish);
+        }
+
+    }
+
+    private void createFishAux(Fish fish) {
+        Vector3 posicao = new Vector3();
+        float randomWidth = MathUtils.random(viewport.getWorldWidth() - 200, 200);
+        float randomHeight = MathUtils.random(viewport.getWorldHeight() - 200, 200);
+
+        fish.setCenter(randomWidth, randomHeight);
+        this.toCapture.add(fish);
+    }
+
+    private void spawnEnemy() {
         Vector2 goalCenter = new Vector2();
         Vector2 spikyGoal = this.mainCharacter
                 .getBoundingRectangle()
@@ -302,16 +293,13 @@ public class UnderwaterCat extends MiniGame {
         enemy.setSpeed(spikySpeed);
         enemies.add(enemy);
     }
-    
 
     // Classes utilizadas
-    
-    
     class TheCat extends AnimatedSprite {
-       
+
         TheCat(final Texture swimCatTexture) {
-                      
-            super(new Animation(0.2f,workFrames(swimCatTexture,8,6)));
+
+            super(new Animation<TextureRegion>(0.2f, workFrames(swimCatTexture, 8, 6)));
             super.getAnimation().setPlayMode(Animation.PlayMode.LOOP);
             super.setAutoUpdate(false);
         }
@@ -321,59 +309,55 @@ public class UnderwaterCat extends MiniGame {
                     this.getX() + this.getWidth(),
                     this.getY() + this.getHeight());
         }
-        }
-    
-   
-    
-class Fish extends AnimatedSprite {
+    }
 
-        
-        Fish(final Texture fishTexture, int rows,int columns) {
-                     
-            super(new Animation(0.2f,workFrames(fishTexture,rows,columns)));
+    class Fish extends AnimatedSprite {
+
+        Fish(final Texture fishTexture, int rows, int columns) {
+
+            super(new Animation<TextureRegion>(0.2f, workFrames(fishTexture, rows, columns)));
             super.getAnimation().setPlayMode(Animation.PlayMode.LOOP);
             super.setAutoUpdate(false);
         }
-        
 
         Vector2 getPosition() {
             return new Vector2(
                     this.getX() + this.getWidth() * 0.5f,
                     this.getY() + this.getHeight() * 0.8f);
         }
-        
+
         float getDistanceTo(float otherbodyX, float otherbodyY) {
-                return getPosition().dst(otherbodyX, otherbodyY);
-            }
-                    
+            return getPosition().dst(otherbodyX, otherbodyY);
         }
-   
+
+    }
+
     class Spiky extends AnimatedSprite {
-        
+
         private Vector2 speed;
         static final int FRAME_WIDTH = 79;
         static final int FRAME_HEIGHT = 87;
-        
+
         Spiky(final Texture spikyTexture) {
-            super(new Animation(0.2f,workFrames(spikyTexture,4,12)));
+            super(new Animation(0.2f, workFrames(spikyTexture, 4, 12)));
             super.getAnimation().setPlayMode(Animation.PlayMode.LOOP);
             super.setAutoUpdate(false);
         }
-        
+
         @Override
         public void update(float dt) {
             super.update(dt);
             super.setPosition(super.getX() + this.speed.x * dt,
                     super.getY() + this.speed.y * dt);
         }
-            
+
         public Vector2 getSpeed() {
             return speed;
         }
-            
+
         public void setSpeed(Vector2 speed) {
             this.speed = speed;
         }
-                 
-    }         
+
+    }
 }
