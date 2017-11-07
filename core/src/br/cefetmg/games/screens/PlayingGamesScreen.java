77b8bb9e226c3f8346ae35f3d2a63c051127f1ef
Adriel Augusto <adrielags@gmail.com>
2017-11-07 16:51:38 -0200

    erroCritico

--- core/src/br/cefetmg/games/screens/PlayingGamesScreen.java
+++ core/src/br/cefetmg/games/screens/PlayingGamesScreen.java
@@ -40,40 +40,40 @@ public class PlayingGamesScreen extends BaseScreen
         sequencer = new GameSequencer(5, new HashSet<MiniGameFactory>(
                 Arrays.asList(
                         // flávio
-                        new ShootTheCariesFactory(),
-                        new ShooTheTartarusFactory(),
+                         //new ShootTheCariesFactory(),
+                         //new ShooTheTartarusFactory(),
                         // gustavo henrique e rogenes
-                        new BasCATballFactory(),
-                        new RunningFactory(),
+                         new BasCATballFactory(),
+                         new RunningFactory(),
                         // rafael e luis carlos
-                        new DodgeTheVeggiesFactory(),
-                        new CatchThatHomeworkFactory(),
+                         new DodgeTheVeggiesFactory(),
+                         new CatchThatHomeworkFactory(),
                         // adriel
-                        new UnderwaterCatFactory(),
+                         new UnderwaterCatFactory(),
                         // arthur e pedro
-                        new DogBarksCatFleeFactory(),
-                        new ClickFindCatFactory(),
+                         new DogBarksCatFleeFactory(),
+                         new ClickFindCatFactory(),
                         // cassiano e gustavo jordão
-                        new TicCatDogFactory(),
-                        new JumpTheObstaclesFactory(),
+                         new TicCatDogFactory(),
+                         new JumpTheObstaclesFactory(),
                         // luiza e pedro cordeiro
-                        new SpyFishFactory(),
-                        new PhantomCatFactory(),
+                         new SpyFishFactory(),
+                         new PhantomCatFactory(),
                         // gabriel e natália
-                        new MouseAttackFactory(),
-                        new JetRatFactory(),
+                         new MouseAttackFactory(),
+                         new JetRatFactory(),
                         // emanoel e vinícius
-                        new HeadSoccerFactory(),
-                        new CatAvoiderFactory(),
+                         new HeadSoccerFactory(),
+                         new CatAvoiderFactory(),
                         // joão e miguel
-                        new CannonCatFactory(),
-                        new MeowsicFactory(),
+                         new CannonCatFactory(),
+                         new MeowsicFactory(),
                         // túlio
-                        new RainingCatsFactory(),
-                        new NinjaCatFactory(),
+                         new RainingCatsFactory(),
+                         new NinjaCatFactory(),
                         //estevao e sarah//
-                        new TheFridgeGameFactory(),
-                        new KillTheRatsFactory()
+                         new TheFridgeGameFactory(),
+                         new KillTheRatsFactory()
                 )
         ), 0, 1, this, this);
         hud = new Hud(this, this);
