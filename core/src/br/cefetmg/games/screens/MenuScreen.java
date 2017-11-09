848b9ae41ba43ebf5ae1e3c575762cd2ababc524
natyiassanori <natsumyiass@gmail.com>
2017-11-09 20:01:38 -0200

    ajustes

--- core/src/br/cefetmg/games/screens/MenuScreen.java
+++ core/src/br/cefetmg/games/screens/MenuScreen.java
@@ -17,40 +17,35 @@ import com.badlogic.gdx.scenes.scene2d.ui.Button;
  *
  * @author Flávio Coutinho - fegemo <coutinho@decom.cefetmg.br>
  */
-
-
-
 public class MenuScreen extends BaseScreen {
 
+        
     public static final int BUTTONS_X = 538;
     public static final int BUTTONS_WIDTH = 204;
     public static final int BUTTONS_HEIGHT = 54;
     public static final int PLAY_Y = 360;
     public static final int RANKING_Y = 276;
     public static final int CREDITS_Y = 192;
-    public static final int WORLD_Y = 108;
-    public static final int EXIT_Y = 24;
-    
+    public static final int EXIT_Y = 108;
+
     public static final int LOGO_X = 160;
     public static final int LOGO_Y = 360;
     public static final int LOGO_WIDTH = 960;
     public static final int LOGO_HEIGHT = 386;
-    
+
     private static final int NUMBER_OF_TILED_BACKGROUND_TEXTURE = 7;
     private TextureRegion background;
     private Texture btnPlay;
     private Texture btnExit;
     private Texture btnRanking;
     private Texture btnCredits;
-    private Texture btnWorld;
     private Texture btnNormal;
     private Texture btnSurvival;
     private Texture btnBack;
     private Texture logo;
-    private boolean selecionaModo = false;
     private Sound click1;
     private Sound click2;
-    
+    private int selecionaModo=0;
 
     /**
      * Cria uma nova tela de menu.
@@ -75,7 +70,6 @@ public class MenuScreen extends BaseScreen {
         btnExit = new Texture("menu/button_sair.png");
         btnRanking = new Texture("menu/button_ranking.png");
         btnCredits = new Texture("menu/button_creditos.png");
-        btnWorld = new Texture("menu/button_mundo.png");
         btnNormal = new Texture("menu/button_normal.png");
         btnSurvival = new Texture("menu/button_survival.png");
         btnBack = new Texture("menu/button_voltar.png");
@@ -91,14 +85,14 @@ public class MenuScreen extends BaseScreen {
         // um número de vezes igual a NUMBER_OF_TILED_BACKGROUND_TEXTURE
         background.setRegionWidth(
                 background.getTexture().getWidth()
-                        * NUMBER_OF_TILED_BACKGROUND_TEXTURE);
+                * NUMBER_OF_TILED_BACKGROUND_TEXTURE);
         // idem para altura, porém será repetida um número de vezes igual a
         // NUMBER_OF_TILED_BACKGROUND_TEXTURE * razãoDeAspecto
         background.setRegionHeight(
                 (int) (background.getTexture().getHeight()
-                        * NUMBER_OF_TILED_BACKGROUND_TEXTURE
-                        / Config.DESIRED_ASPECT_RATIO));
-        
+                * NUMBER_OF_TILED_BACKGROUND_TEXTURE
+                / Config.DESIRED_ASPECT_RATIO));
+
     }
 
     /**
@@ -106,76 +100,63 @@ public class MenuScreen extends BaseScreen {
      */
     @Override
     public void handleInput() {
-       
-        
+
         //verifica se clique foi em algum botão
         if (Gdx.input.justTouched()) {
-            Vector3 tmp=new Vector3(Gdx.input.getX(),Gdx.input.getY(), 0);
-            camera.unproject(tmp);
-            Rectangle playBounds=new Rectangle(BUTTONS_X, PLAY_Y, BUTTONS_WIDTH,BUTTONS_HEIGHT);
-            Rectangle rankingBounds=new Rectangle(BUTTONS_X, RANKING_Y, BUTTONS_WIDTH,BUTTONS_HEIGHT);
-            Rectangle creditsBounds=new Rectangle(BUTTONS_X, CREDITS_Y, BUTTONS_WIDTH,BUTTONS_HEIGHT);
-            Rectangle worldBounds=new Rectangle(BUTTONS_X, WORLD_Y, BUTTONS_WIDTH,BUTTONS_HEIGHT);
-            Rectangle exitBounds=new Rectangle(BUTTONS_X, EXIT_Y, BUTTONS_WIDTH,BUTTONS_HEIGHT);
-            
-            
-            if(!selecionaModo){
-                if(playBounds.contains(tmp.x,tmp.y)){
-                    selecionaModo = true;
+            Vector3 clickPosition = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(clickPosition);
+            Rectangle playBounds = new Rectangle(BUTTONS_X, PLAY_Y, BUTTONS_WIDTH, BUTTONS_HEIGHT);
+            Rectangle rankingBounds = new Rectangle(BUTTONS_X, RANKING_Y, BUTTONS_WIDTH, BUTTONS_HEIGHT);
+            Rectangle creditsBounds = new Rectangle(BUTTONS_X, CREDITS_Y, BUTTONS_WIDTH, BUTTONS_HEIGHT);
+            Rectangle exitBounds = new Rectangle(BUTTONS_X, EXIT_Y, BUTTONS_WIDTH, BUTTONS_HEIGHT);
+
+            if (selecionaModo==0) {
+                if (playBounds.contains(clickPosition.x, clickPosition.y)) {
+                    selecionaModo = 1;
                     click2.play();
                 }
-                if(rankingBounds.contains(tmp.x,tmp.y)){
+                if (rankingBounds.contains(clickPosition.x, clickPosition.y)) {
                     /*
                     
                     CHAMADA DA TELA DE RANKING
                     
-                    */
-                    
+                     */
+
                     click2.play();
                 }
-                if(creditsBounds.contains(tmp.x,tmp.y)){
+                if (creditsBounds.contains(clickPosition.x, clickPosition.y)) {
                     /*
                     
                     CHAMADA DA TELA DE CRÉDITOS
                     
-                    */
+                     */
                     click2.play();
                 }
-                if(worldBounds.contains(tmp.x,tmp.y)){
-                    /*
-                    
-                    CHAMADA DA TELA OVERWORLD
-                    
-                    
-                    */
-                    click2.play();
-                }                
-                if(exitBounds.contains(tmp.x,tmp.y)){
+                if (exitBounds.contains(clickPosition.x, clickPosition.y)) {
                     click1.play();
                     System.exit(0);
                 }
-            }
-            else{
-                if(playBounds.contains(tmp.x,tmp.y)){
+            } else {
+                if (playBounds.contains(clickPosition.x, clickPosition.y)) {
                     //CHAMADA DO MODO NORMAL
                     click2.play();
                     navigateToMicroGameScreen();
                 }
-                if(rankingBounds.contains(tmp.x,tmp.y)){
+                if (rankingBounds.contains(clickPosition.x, clickPosition.y)) {
                     /*
                     
                     CHAMADA DO MODO SURVIVAL
                     
-                    */
+                     */
                     click2.play();
                 }
-                if(creditsBounds.contains(tmp.x,tmp.y)){
+                if (creditsBounds.contains(clickPosition.x, clickPosition.y)) {
                     //Volta para os botões
-                    selecionaModo = false;
+                    selecionaModo = 0;
                     click1.play();
                 }
             }
-            
+
         }
     }
 
@@ -188,8 +169,7 @@ public class MenuScreen extends BaseScreen {
     public void update(float dt) {
         float speed = dt * 0.25f;
         background.scroll(speed, -speed);
-        
-        
+
     }
 
     /**
@@ -202,30 +182,26 @@ public class MenuScreen extends BaseScreen {
                 viewport.getWorldWidth(),
                 viewport.getWorldHeight());
         batch.draw(logo, LOGO_X, LOGO_Y,
-                LOGO_WIDTH,LOGO_HEIGHT);
-        
-        if(!selecionaModo){
+                LOGO_WIDTH, LOGO_HEIGHT);
+
+        if (selecionaModo == 0) {
             batch.draw(btnPlay, BUTTONS_X, PLAY_Y,
-                BUTTONS_WIDTH,BUTTONS_HEIGHT);
+                    BUTTONS_WIDTH, BUTTONS_HEIGHT);
             batch.draw(btnRanking, BUTTONS_X, RANKING_Y,
-                BUTTONS_WIDTH,BUTTONS_HEIGHT);
+                    BUTTONS_WIDTH, BUTTONS_HEIGHT);
             batch.draw(btnCredits, BUTTONS_X, CREDITS_Y,
-                BUTTONS_WIDTH,BUTTONS_HEIGHT);
-            batch.draw(btnWorld, BUTTONS_X, WORLD_Y,
-                BUTTONS_WIDTH,BUTTONS_HEIGHT);
+                    BUTTONS_WIDTH, BUTTONS_HEIGHT);
             batch.draw(btnExit, BUTTONS_X, EXIT_Y,
-                BUTTONS_WIDTH,BUTTONS_HEIGHT);
-        }
-        else{
+                    BUTTONS_WIDTH, BUTTONS_HEIGHT);
+        } else if(selecionaModo == 1) {
             batch.draw(btnNormal, BUTTONS_X, PLAY_Y,
-                BUTTONS_WIDTH,BUTTONS_HEIGHT);
+                    BUTTONS_WIDTH, BUTTONS_HEIGHT);
             batch.draw(btnSurvival, BUTTONS_X, RANKING_Y,
-                BUTTONS_WIDTH,BUTTONS_HEIGHT);
+                    BUTTONS_WIDTH, BUTTONS_HEIGHT);
             batch.draw(btnBack, BUTTONS_X, CREDITS_Y,
-                BUTTONS_WIDTH,BUTTONS_HEIGHT);
+                    BUTTONS_WIDTH, BUTTONS_HEIGHT);
         }
-        
-       
+
         /*drawCenterAlignedText("Pressione qualquer tecla para jogar",
                 viewport.getWorldHeight() * 0.35f);*/
         batch.end();
@@ -237,7 +213,6 @@ public class MenuScreen extends BaseScreen {
     private void navigateToMicroGameScreen() {
         game.setScreen(new PlayingGamesScreen(game, this));
     }
-
 
     /**
      * Libera os recursos necessários para esta tela.
