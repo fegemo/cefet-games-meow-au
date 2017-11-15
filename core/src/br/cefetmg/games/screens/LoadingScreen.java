241480649cf19e76fe7d36b4e0fc643fa6e51927
Flavio Coutinho <fegemo@gmail.com>
2017-11-15 20:37:44 -0200

    Changes the implementation of the cooldown timer on LoadingScreen

--- core/src/br/cefetmg/games/screens/LoadingScreen.java
+++ core/src/br/cefetmg/games/screens/LoadingScreen.java
@@ -4,27 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Pixmap.Blending;
-import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-//REFERÊNCIA DESTE CÓDIGO: https://github.com/serhiy/libgdx-circular-cooldown
 public class LoadingScreen {
 
     private final Stage stage;
-    private final CooldownTimer cooldownTimerBlue;
+    private final CooldownTimer cooldownTimer;
 
-    private long lastUpdate = 0L;
     private float remainingPercentage = 1.0f;
 
     private final float STEP = 0.05f;
@@ -35,12 +28,12 @@ public class LoadingScreen {
     public LoadingScreen() {
         stage = new Stage();
 
-        cooldownTimerBlue = new CooldownTimer(true);
-        cooldownTimerBlue.setSize(TIMER_SIZE, TIMER_SIZE);
-        cooldownTimerBlue.setPosition(0, 0);
-        cooldownTimerBlue.setColor(Color.BLUE);
+        cooldownTimer = new CooldownTimer(true);
+        cooldownTimer.setSize(TIMER_SIZE, TIMER_SIZE);
+        cooldownTimer.setPosition(0, 0);
+        cooldownTimer.setColor(0.75f, 0.64f, 0.8f, 1);
 
-        stage.addActor(cooldownTimerBlue);
+        stage.addActor(cooldownTimer);
 
         // instancia a textura e a região de textura
         Texture backgroundTexture = new Texture("loading-page.jpg");
@@ -50,23 +43,23 @@ public class LoadingScreen {
         background = new TextureRegion(backgroundTexture);
     }
 
-    public boolean draw(AssetManager assets, SpriteBatch batch, Viewport viewport) {
+    public boolean draw(AssetManager assets, SpriteBatch batch,
+            Viewport viewport) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        batch.draw(background, 0, TIMER_SIZE, viewport.getScreenWidth(), viewport.getScreenHeight() - TIMER_SIZE);
+        batch.draw(background, 0, TIMER_SIZE,
+                viewport.getScreenWidth(),
+                viewport.getScreenHeight() - TIMER_SIZE);
         batch.end();
 
-        if (System.currentTimeMillis() - lastUpdate > 25L) {
-            cooldownTimerBlue.update(remainingPercentage);
+        cooldownTimer.update(remainingPercentage);
 
-            //A medida que os assets são carregados, atualiza-se suavemente o círculo de carregamento
-            if (remainingPercentage - STEP >= 1 - assets.getProgress()) {
-                remainingPercentage -= STEP;
-            }
-
-            lastUpdate = System.currentTimeMillis();
+        //A medida que os assets são carregados, atualiza-se suavemente o 
+        // círculo de carregamento
+        if (remainingPercentage - STEP >= 1 - assets.getProgress()) {
+            remainingPercentage -= STEP;
         }
 
         stage.act();
@@ -79,37 +72,67 @@ public class LoadingScreen {
         return false;
     }
 
-    public class CooldownTimer extends Table {
+    public class CooldownTimer extends Actor {
 
+        private static final int TOTAL_SEGMENTS = 300;
         private static final float START_ANGLE = 90;
-
-        private final boolean clockwise;
-
-        private final Table cooldownDisplay;
-        private TextureRegionDrawable cooldownTexture;
-
+        private static final float INITIAL_RADIUS = 15;
+        private static final float RADIUS_VARIATION = 1.5f;
+        private float totalAngle = 360;
+        private float radius = INITIAL_RADIUS;
+        private final ShapeRenderer shapes;
         private float alpha = 0.5f;
+        private float percentageComplete;
+        private float ellapsedTime = 0;
 
         /**
          * @param clockwise determines the rotation side of the cooldown timer.
          */
         public CooldownTimer(boolean clockwise) {
-            this.clockwise = clockwise;
+            this.shapes = new ShapeRenderer();
+            this.percentageComplete = 0;
+            if (clockwise) {
+                totalAngle *= -1;
+            }
+        }
 
-            cooldownDisplay = new Table();
-            cooldownDisplay.setPosition(0, 0);
-            addActor(cooldownDisplay);
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            batch.end();
+            shapes.setProjectionMatrix(batch.getProjectionMatrix());
+            shapes.setTransformMatrix(batch.getTransformMatrix());
+            shapes.begin(ShapeRenderer.ShapeType.Filled);
+            shapes.setColor(super.getColor());
+            shapes.arc(
+                    INITIAL_RADIUS + RADIUS_VARIATION,
+                    INITIAL_RADIUS + RADIUS_VARIATION,
+                    radius,
+                    START_ANGLE,
+                    totalAngle * percentageComplete,
+                    TOTAL_SEGMENTS);
+            shapes.end();
+            shapes.begin(ShapeRenderer.ShapeType.Line);
+            shapes.setColor(Color.GRAY);
+            shapes.circle(
+                    INITIAL_RADIUS + RADIUS_VARIATION,
+                    INITIAL_RADIUS + RADIUS_VARIATION,
+                    radius, TOTAL_SEGMENTS);
+            shapes.end();
+            batch.begin();
         }
 
         /**
          * @param remainingPercentage to be rendered by cooldown timer.
          */
         public void update(float remainingPercentage) {
-            cooldownDisplay.clear();
+            this.percentageComplete = 1 - remainingPercentage;
+        }
 
-            Image cooldownTimer = new Image(cooldownTimer(remainingPercentage));
-            cooldownTimer.setColor(1.0f, 1.0f, 1.0f, alpha);
-            cooldownDisplay.addActor(cooldownTimer);
+        @Override
+        public void act(float delta) {
+            ellapsedTime += delta;
+            this.radius = (float) (Math.sin(ellapsedTime) * RADIUS_VARIATION)
+                    + INITIAL_RADIUS;
         }
 
         public float getAlpha() {
@@ -121,66 +144,6 @@ public class LoadingScreen {
          */
         public void setAlpha(float alpha) {
             this.alpha = alpha;
-        }
-
-        private Drawable cooldownTimer(float remainingPercentage) {
-            if (remainingPercentage > 1.0f || remainingPercentage < 0.0f) {
-                return null;
-            }
-
-            float radius = Math.min(getWidth() / 2, getHeight() / 2);
-            float angle = calculateAngle(remainingPercentage);
-            int segments = calculateSegments(angle);
-
-            Pixmap display = new Pixmap((int) getWidth(), (int) getHeight(), Format.RGBA8888);
-
-            Blending blending = display.getBlending();
-
-            try {
-                float theta = (2 * MathUtils.PI * (angle / 360.0f)) / segments;
-                float cos = MathUtils.cos(theta);
-                float sin = MathUtils.sin(theta);
-                float cx = radius * MathUtils.cos(START_ANGLE * MathUtils.degreesToRadians);
-                float cy = radius * MathUtils.sin((-1 * START_ANGLE) * MathUtils.degreesToRadians);
-
-                display.setColor(getColor());
-
-                for (int count = 0; count < segments; count++) {
-                    float pcx = cx;
-                    float pcy = cy;
-                    float temp = cx;
-                    cx = cos * cx - sin * cy;
-                    cy = sin * temp + cos * cy;
-                    display.fillTriangle((int) getWidth() / 2, (int) getHeight() / 2, (int) (getWidth() / 2 + pcx), (int) (getHeight() / 2 + pcy), (int) (getWidth() / 2 + cx), (int) (getHeight() / 2
-                            + cy));
-                }
-
-                display.setBlending(Blending.None);
-
-                if (cooldownTexture == null) {
-                    cooldownTexture = new TextureRegionDrawable(new TextureRegion(new Texture(display)));
-                } else {
-                    cooldownTexture.getRegion().getTexture().draw(display, 0, 0);
-                }
-
-                return cooldownTexture;
-            } finally {
-                display.setBlending(blending);
-
-                display.dispose();
-            }
-        }
-
-        private float calculateAngle(float remainingPercentage) {
-            if (clockwise) {
-                return 360 - 360 * remainingPercentage;
-            } else {
-                return 360 * remainingPercentage - 360;
-            }
-        }
-
-        private int calculateSegments(float angle) {
-            return Math.max(1, (int) (6 * (float) Math.cbrt(Math.abs(angle)) * (Math.abs(angle) / 360.0f)));
         }
     }
 }
