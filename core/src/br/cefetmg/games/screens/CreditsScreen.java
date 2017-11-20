78fe8eed7e53de0ad286fb213e11e8a948b7253b
GabrielMelo96 <gabrielsilvamelo96@gmail.com>
2017-11-20 18:02:06 -0200

    Tela de creditos: Adicionando tempo e background

--- core/src/br/cefetmg/games/screens/CreditsScreen.java
+++ core/src/br/cefetmg/games/screens/CreditsScreen.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader.FreeTypeFontLoaderParameter;
 import com.badlogic.gdx.math.Vector2;
@@ -22,11 +23,16 @@ import java.util.List;
  *
  * @author Isaac
  */
+ 
+
 public class CreditsScreen extends BaseScreen{
     private BitmapFont font;
     private List<String> lines; 
     private List<Vector2> linePos;
+    static int SPACING = 50;
     FreeTypeFontGenerator generator;
+    private TextureRegion background;
+
     public CreditsScreen(Game game, BaseScreen previous) {
         super(game, previous);
         
@@ -36,8 +42,9 @@ public class CreditsScreen extends BaseScreen{
     public void appear() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         //init font
+        background = new TextureRegion(new Texture("creditsScreen/credits-background.jpg"));
         FreeTypeFontLoaderParameter snackerComicParams = new FreeTypeFontLoaderParameter();
-        snackerComicParams.fontFileName = "fonts/snacker-comic.ttf";
+        snackerComicParams.fontFileName = "fonts/sawasdee-50.fnt";
         snackerComicParams.fontParameters.size = 50;
         snackerComicParams.fontParameters.minFilter = Texture.TextureFilter.Linear;
         snackerComicParams.fontParameters.magFilter = Texture.TextureFilter.Linear;
@@ -54,7 +61,7 @@ public class CreditsScreen extends BaseScreen{
                 while ((line = br.readLine()) != null) {
                     lines.add(line);
                     linePos.add(new Vector2((float) (Gdx.graphics.getWidth()*0.5),aux));
-                    aux-=50;
+                    aux-=SPACING;
                 }
                 br.close();
     } 
@@ -79,18 +86,25 @@ public class CreditsScreen extends BaseScreen{
        
 
         for ( int i=0; i<lines.size();i++){
-            linePos.get(i).y++;
+            linePos.get(i).y+=2;
+        }
+
+        if(linePos.get(lines.size()-1).y>Gdx.graphics.getHeight()+lines.size()*SPACING){
+               game.setScreen(new MenuScreen(game, this));
         }
     }
 
     @Override
     public void draw() {
              font = assets.get("snacker-comic-50.ttf");
-             font.setColor(Color.RED);
+             font.setColor(Color.GREEN);
              batch.begin();
+             batch.draw(background, 0, 0,
+             viewport.getWorldWidth(),
+             viewport.getWorldHeight());
              for ( int i=0; i<lines.size();i++){
              //    if(lines.get(i).getChars(0, i, dst, i))
-              //     font.draw(batch, lines.get(i), linePos.get(i).x, linePos.get(i).y);
+                   font.draw(batch, lines.get(i), linePos.get(i).x, linePos.get(i).y);
              }
              batch.end();
     }
