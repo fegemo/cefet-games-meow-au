bf4f3c6ab16aa3dfa93f23867494f26e329ae0fd
Andre Brait <andrebrait@gmail.com>
2017-12-03 00:05:37 -0200

    Adicionando Scoreboard compartilhado para Android

--- android/src/br/cefetmg/games/AndroidLauncher.java
+++ android/src/br/cefetmg/games/AndroidLauncher.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import br.cefetmg.games.MeowAuGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -12,6 +11,6 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useImmersiveMode = true;
-		initialize(new MeowAuGame(null), config);
+		initialize(new MeowAuGame(new AndroidLeaderboard()), config);
 	}
 }
